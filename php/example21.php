e74e27f3b3028feebdef83d5be4c62b327178e46
Sebastion Price <sebastionprice@gmail.com>
2025-02-08 08:58:19 +1100

    misc php tutorial updates

---
+++ php/example21.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>web</title>
+  </head>
+  <body>
+
+      <?php
+      function sayHi(){
+        echo "string";
+      }
+       ?>
+
+  </body>
+</html>
