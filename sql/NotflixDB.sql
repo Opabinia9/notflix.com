822630dfaefeed4d93dea0a360e352474a750f09
Sebastion Price <sebastionprice@gmail.com>
2025-02-10 21:45:31 +1100

    added notflix database

---
+++ sql/NotflixDB.sql
@@ -0,0 +1,112 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Linux (x86_64)
+--
+-- Host: localhost    Database: NotflixDB
+-- ------------------------------------------------------
+-- Server version	8.0.41-0ubuntu0.24.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Episode`
+--
+
+DROP TABLE IF EXISTS `Episode`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Episode` (
+  `SeriesID` int NOT NULL,
+  `SeasonID` int NOT NULL,
+  `EpisodeID` int NOT NULL,
+  `EpisodeTitle` varchar(50) DEFAULT NULL,
+  `Length` varchar(7) DEFAULT NULL,
+  `Available` tinyint(1) DEFAULT '0',
+  `URL` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`SeriesID`,`SeasonID`,`EpisodeID`),
+  KEY `SeasonID` (`SeasonID`),
+  CONSTRAINT `Episode_ibfk_1` FOREIGN KEY (`SeriesID`) REFERENCES `Media` (`SeriesID`),
+  CONSTRAINT `Episode_ibfk_2` FOREIGN KEY (`SeasonID`) REFERENCES `Season` (`SeasonID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Episode`
+--
+
+LOCK TABLES `Episode` WRITE;
+/*!40000 ALTER TABLE `Episode` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Episode` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Media`
+--
+
+DROP TABLE IF EXISTS `Media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Media` (
+  `SeriesID` int NOT NULL AUTO_INCREMENT,
+  `Title` varchar(50) DEFAULT NULL,
+  `ReleaseYear` int DEFAULT NULL,
+  `Length` varchar(7) DEFAULT NULL,
+  `NOS` int DEFAULT '0',
+  `Available` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`SeriesID`),
+  CONSTRAINT `Media_chk_1` CHECK ((`NOS` >= 0))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Media`
+--
+
+LOCK TABLES `Media` WRITE;
+/*!40000 ALTER TABLE `Media` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Season`
+--
+
+DROP TABLE IF EXISTS `Season`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Season` (
+  `seriesID` int NOT NULL,
+  `SeasonID` int NOT NULL,
+  `NOE` int DEFAULT NULL,
+  PRIMARY KEY (`seriesID`,`SeasonID`),
+  KEY `idx_seasonid` (`SeasonID`),
+  CONSTRAINT `Season_ibfk_1` FOREIGN KEY (`seriesID`) REFERENCES `Media` (`SeriesID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Season`
+--
+
+LOCK TABLES `Season` WRITE;
+/*!40000 ALTER TABLE `Season` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Season` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-10 21:44:42
