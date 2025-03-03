c70d2cac4548f58c4976cde67ad9ff6a3adcd1e1
Sebastion Price <sebastionprice@gmail.com>
2025-03-03 12:01:00 +1100

    UPDATED: Database

--- sql/NotflixDB.sql
+++ sql/NotflixDB.sql
@@ -29,8 +29,8 @@ CREATE TABLE `bugrep` (
   `ReportDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`BID`),
   KEY `AccountID` (`AccountID`),
-  CONSTRAINT `bugrep_ibfk_1` FOREIGN KEY (`AccountID`) REFERENCES `users` (`AccountID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `bugrep_ibfk_1` FOREIGN KEY (`AccountID`) REFERENCES `users` (`AccountID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +39,6 @@ CREATE TABLE `bugrep` (
 
 LOCK TABLES `bugrep` WRITE;
 /*!40000 ALTER TABLE `bugrep` DISABLE KEYS */;
-INSERT INTO `bugrep` VALUES (1,2,'error\r\n','2025-03-02 22:32:32');
 /*!40000 ALTER TABLE `bugrep` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,9 +58,9 @@ CREATE TABLE `comments` (
   PRIMARY KEY (`CID`),
   KEY `comments_ibfk_1` (`AccountID`),
   KEY `comments_ibfk_2` (`SeriesID`),
-  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`AccountID`) REFERENCES `users` (`AccountID`),
-  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`SeriesID`) REFERENCES `media` (`SeriesID`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`AccountID`) REFERENCES `users` (`AccountID`) ON DELETE CASCADE,
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`SeriesID`) REFERENCES `media` (`SeriesID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,8 +90,8 @@ CREATE TABLE `episode` (
   PRIMARY KEY (`SeriesID`,`SeasonID`,`EpisodeID`),
   KEY `SeasonID` (`SeasonID`),
   KEY `watchlist_ibfk_4` (`EpisodeID`),
-  CONSTRAINT `Episode_ibfk_1` FOREIGN KEY (`SeriesID`) REFERENCES `media` (`SeriesID`),
-  CONSTRAINT `Episode_ibfk_2` FOREIGN KEY (`SeasonID`) REFERENCES `season` (`SeasonID`)
+  CONSTRAINT `Episode_ibfk_1` FOREIGN KEY (`SeriesID`) REFERENCES `media` (`SeriesID`) ON DELETE CASCADE,
+  CONSTRAINT `Episode_ibfk_2` FOREIGN KEY (`SeasonID`) REFERENCES `season` (`SeasonID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -102,7 +101,6 @@ CREATE TABLE `episode` (
 
 LOCK TABLES `episode` WRITE;
 /*!40000 ALTER TABLE `episode` DISABLE KEYS */;
-INSERT INTO `episode` VALUES (1,0,1,'testmovie','1h 24m',1,'http://notflix.com/videos/examplemovie.mp4'),(2,0,1,'a second movie','0h 0m',1,'http://notflix.com/'),(3,1,1,'the fisrt episode','45m',1,'http://notflix.com/videos/S1E1.mp4'),(3,1,2,'episode 2','45m',1,NULL),(3,1,3,'and another','45m',1,NULL),(3,1,4,'end','45m',1,NULL),(3,2,1,'welcome to season 2','45m',1,NULL),(3,2,2,'it continue','45m',1,'http://notflix.com/videos/S1E1.mp4'),(3,2,3,'almost done','45m',1,NULL),(3,2,4,'the end','45m',1,NULL);
 /*!40000 ALTER TABLE `episode` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,8 +118,8 @@ CREATE TABLE `features` (
   `RequestDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`FID`),
   KEY `AccountID` (`AccountID`),
-  CONSTRAINT `features_ibfk_1` FOREIGN KEY (`AccountID`) REFERENCES `users` (`AccountID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `features_ibfk_1` FOREIGN KEY (`AccountID`) REFERENCES `users` (`AccountID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -130,7 +128,6 @@ CREATE TABLE `features` (
 
 LOCK TABLES `features` WRITE;
 /*!40000 ALTER TABLE `features` DISABLE KEYS */;
-INSERT INTO `features` VALUES (1,2,NULL,'2025-03-02 22:20:36'),(2,2,NULL,'2025-03-02 22:22:11'),(3,2,'vcdsz','2025-03-02 22:23:16'),(4,2,'bvcxz','2025-03-02 22:23:32');
 /*!40000 ALTER TABLE `features` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -152,7 +149,7 @@ CREATE TABLE `media` (
   `Poster` varchar(255) DEFAULT 'http://notflix.com/imgs/default.jpg',
   PRIMARY KEY (`SeriesID`),
   CONSTRAINT `media_chk_1` CHECK ((`NOS` >= 0))
-) ENGINE=InnoDB AUTO_INCREMENT=102 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -161,7 +158,6 @@ CREATE TABLE `media` (
 
 LOCK TABLES `media` WRITE;
 /*!40000 ALTER TABLE `media` DISABLE KEYS */;
-INSERT INTO `media` VALUES (1,'testmovie','this is a desc',1993,'1h 24m',0,1,'http://notflix.com/imgs/xfiles.jpg'),(2,'a second movie','a movie that is a sequl to the not aclaimed \"testmovie\"',2024,'0h 0m',0,1,'http://notflix.com/imgs/default.jpg'),(3,'the series','the spin off series of the underaarated testmovie series',2025,'99h 99m',2,1,'http://notflix.com/imgs/show.jpg');
 /*!40000 ALTER TABLE `media` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -179,8 +175,8 @@ CREATE TABLE `requests` (
   `RequestDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`RID`),
   KEY `AccountID` (`AccountID`),
-  CONSTRAINT `requests_ibfk_1` FOREIGN KEY (`AccountID`) REFERENCES `users` (`AccountID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `requests_ibfk_1` FOREIGN KEY (`AccountID`) REFERENCES `users` (`AccountID`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -189,7 +185,6 @@ CREATE TABLE `requests` (
 
 LOCK TABLES `requests` WRITE;
 /*!40000 ALTER TABLE `requests` DISABLE KEYS */;
-INSERT INTO `requests` VALUES (1,2,'sad','2025-03-02 21:30:12'),(2,2,'hello','2025-03-02 22:03:06'),(3,2,'and another\r\n','2025-03-02 22:03:18'),(4,2,'test again','2025-03-02 22:06:39');
 /*!40000 ALTER TABLE `requests` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -206,7 +201,7 @@ CREATE TABLE `season` (
   `NOE` int DEFAULT NULL,
   PRIMARY KEY (`seriesID`,`SeasonID`),
   KEY `idx_seasonid` (`SeasonID`),
-  CONSTRAINT `Season_ibfk_1` FOREIGN KEY (`seriesID`) REFERENCES `media` (`SeriesID`)
+  CONSTRAINT `Season_ibfk_1` FOREIGN KEY (`seriesID`) REFERENCES `media` (`SeriesID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -216,7 +211,6 @@ CREATE TABLE `season` (
 
 LOCK TABLES `season` WRITE;
 /*!40000 ALTER TABLE `season` DISABLE KEYS */;
-INSERT INTO `season` VALUES (1,0,1),(2,0,1),(3,1,4),(3,2,4);
 /*!40000 ALTER TABLE `season` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -235,7 +229,7 @@ CREATE TABLE `users` (
   `Salt` varbinary(16) DEFAULT NULL,
   PRIMARY KEY (`AccountID`),
   UNIQUE KEY `Username` (`Username`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -244,7 +238,6 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (2,'seb','$2y$12$GeFQG3gDGUXYhREh/ClrIelaim/zv5Ivezbi5Mp146Irgg4AF9CZG','2025-02-28',_binary '\Â)toô1\ÃEh∫N_òπΩ'),(3,'newac','$2y$12$aRDeyZgjC31YeMuHd2A5deczV16ZtIxW9RGcuPo7LC2SJ1QVAFG2.','2025-03-02',_binary 'ˇ:Fåó\Ê5ù	\"∑\‚');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -265,10 +258,10 @@ CREATE TABLE `watchlist` (
   KEY `SeriesID` (`SeriesID`),
   KEY `SeasonID` (`SeasonID`),
   KEY `EpisodeID` (`EpisodeID`),
-  CONSTRAINT `watchlist_ibfk_1` FOREIGN KEY (`AccountID`) REFERENCES `users` (`AccountID`),
-  CONSTRAINT `watchlist_ibfk_2` FOREIGN KEY (`SeriesID`) REFERENCES `episode` (`SeriesID`),
-  CONSTRAINT `watchlist_ibfk_3` FOREIGN KEY (`SeasonID`) REFERENCES `episode` (`SeasonID`),
-  CONSTRAINT `watchlist_ibfk_4` FOREIGN KEY (`EpisodeID`) REFERENCES `episode` (`EpisodeID`)
+  CONSTRAINT `watchlist_ibfk_1` FOREIGN KEY (`AccountID`) REFERENCES `users` (`AccountID`) ON DELETE CASCADE,
+  CONSTRAINT `watchlist_ibfk_2` FOREIGN KEY (`SeriesID`) REFERENCES `episode` (`SeriesID`) ON DELETE CASCADE,
+  CONSTRAINT `watchlist_ibfk_3` FOREIGN KEY (`SeasonID`) REFERENCES `episode` (`SeasonID`) ON DELETE CASCADE,
+  CONSTRAINT `watchlist_ibfk_4` FOREIGN KEY (`EpisodeID`) REFERENCES `episode` (`EpisodeID`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -278,7 +271,6 @@ CREATE TABLE `watchlist` (
 
 LOCK TABLES `watchlist` WRITE;
 /*!40000 ALTER TABLE `watchlist` DISABLE KEYS */;
-INSERT INTO `watchlist` VALUES (2,1,0,1,'00:00:10'),(2,3,1,1,'00:00:58');
 /*!40000 ALTER TABLE `watchlist` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -291,4 +283,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-03 11:21:07
+-- Dump completed on 2025-03-03 12:00:11
